e1b57445497c0115ea6261cfcd86aa13f17246b1
akshat09022002 <akshat09022002@gmail.com>
2024-01-27 23:43:40 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app=express()
+
+app.use(express.json());
+
+const jwt=require("jsonwebtoken");
+const JWT_TOKEN=require("./config");
+
+const cors=require('cors')
+app.use(cors());
+
+const {userRoute} = require('./routes/user');
+const { accountRouter } = require("./routes/account");
+app.use('/api/v1/user',userRoute);
+app.use('/api/v1/account',accountRouter);
+
+
+
+
+app.listen(3000);
+
