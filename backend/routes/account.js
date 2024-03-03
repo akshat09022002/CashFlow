e21e75d6c9ee02ea663548627505401e044c4502
Akshat Singh <64846352+akshat09022002@users.noreply.github.com>
2024-03-03 21:59:34 +0530

    Update account.js

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -3,7 +3,9 @@ const { account } = require('../db');
 const { authMiddleware } = require('../middleware');
 const mongoose=require('mongoose');
 const app = express();
+const cors=require('cors')
 
+app.use(cors());
 app.use(express.json());
 
 const router= express.Router();
